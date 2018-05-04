276ff7e5d04b5a86506b4a149ddef946d19f19d7
cannotdescribe <952376923@qq.com>
2018-05-04 15:13:52 +0800

    first

---
+++ sc-zuul-first/sc-zuul-first-consumer/src/main/java/com/twilight/springcloud/zuul/consumer/controller/ConsumerController.java
@@ -0,0 +1,52 @@
+package com.twilight.springcloud.zuul.consumer.controller;
+
+import com.twilight.springcloud.zuul.consumer.model.OrderModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author xujin
+ *
+ */
+@RestController
+@RequestMapping("/sc")
+public class ConsumerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+
+    /**
+     * 演示普通路由
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/{id}")
+    public OrderModel getOrderInfo(@PathVariable Long id) {
+        return this.restTemplate.getForObject("http://provider1/" + "/sc/order/" + id, OrderModel.class);
+    }
+
+    @GetMapping("/log-user-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("sc-zuul-first-consumer");
+        // 打印当前选择的是哪个节点
+        logger.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(),
+                serviceInstance.getPort());
+    }
+
+}
